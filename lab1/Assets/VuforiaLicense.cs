2861a25ce712e5928bd5b7375747966c2cb0c4f2
IST-20-1-Artem-Shyrymeta <104629881+IST-20-1-Artem-Shyrymeta@users.noreply.github.com>
2023-08-27 13:11:50 +0300

    Add files via upload

---
+++ lab1/Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QWFFeVppYi8vLy8vQUFBQm1XMGdieUU4RzAvbWpuMFZNYzR5MUQxRUFLbElPa1FudURKeVh3amhtVzVEN3FJZjZLTXh4ankwdk03MmFObXM1TUxwckNrM2RzOXNUaHlDY3M4U3FIcWMxZlBob1ZQWGdrdmxXSkhJM0xKbGNQMnp1YWMrK2RGSkRHa2YxM21aN0ZRbkJkUmIrWlF5KzBralBUV2daMDRYeDhrS1lEODlPTVNOZnlmNEtMUmtyUkRMVjgwcGsyc3ZiYjhadEhpdEpHeURQVFcrMGhLUC9DSWtFVUNBMFlrNUlDRXBScVFJZzByNlpBR0drNk03TVlCbS9kSDlERGVWaWs0eXRkTzVwbE9nR3g0OTJCSVVibXJ0RlZiSW8wYjJOSGhzdlY5VEpYMklXaGYzS0JwandRcVF1RzdueDI1UlhzMmMvYWJ1Rm5pYWRnZDJEbnRxQXJEMlBMbGJNcGtrQjM4YnQrUldHdXIvU0V4N0pQNGw=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
